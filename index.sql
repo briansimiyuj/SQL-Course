d16e5d59ca7800256c7604f047786c3b203052cd
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-10 15:21:49 +0300

    Creating a table

--- index.sql
+++ index.sql
@@ -1,1 +1,10 @@
-SHOW DATABASES;
+CREATE TABLE employees(
+
+    employee_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    hourly_rate DECIMAL(5, 2),
+    hire_date DATE
+
+    
+)
