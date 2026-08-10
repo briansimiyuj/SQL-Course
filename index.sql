e010ba9f3ceac0ce1ad96b891da5038aa7166cae
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-10 15:31:54 +0300

    Selecting a table

--- index.sql
+++ index.sql
@@ -1,10 +1,1 @@
-CREATE TABLE employees(
-
-    employee_id INT,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    hourly_rate DECIMAL(5, 2),
-    hire_date DATE
-
-    
-)
+SELECT * FROM employees;
