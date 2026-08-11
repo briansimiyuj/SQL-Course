6ebf847b814bb1a3327911eebd196ae8d6c86f46
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-11 12:33:09 +0300

    Querying something specific from the table 2

--- index.sql
+++ index.sql
@@ -1,3 +1,11 @@
-SELECT first_name, last_name FROM employees
+SELECT * FROM employees
 
-WHERE employee_id = 1;
+WHERE first_name = 'John';
+
+WHERE hourly_rate > 20;
+
+WHERE hire_date >= '2021-06-15';
+
+WHERE employee_id != 1;
+
+WHERE hire_date is NULL;
