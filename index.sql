c0be30c7874a3e925c108c4fecee428dc19b9acd
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-11 10:40:37 +0300

    Inserting rows into a table

--- index.sql
+++ index.sql
@@ -1,3 +1,5 @@
-ALTER TABLE employees
+INSERT INTO employees
 
-DROP COLUMN home_address;
+VALUES ('mike@gmail.com', 6, 'Mike', 'Doe', '25.50', '2021-06-15', '0703220499'), ('mary@gmail.com', 7, 'Mary', 'Doe', '25.50', '2021-06-15', '0703220499'), ('jane@gmail.com', 1, 'Jane', 'Doe', '25.50', '2021-06-15', '0703220499'), ('joe@gmail.com', 3, 'Joe', 'Doe', '25.50', '2021-06-15', '0703220499'), ('jim@gmail.com', 4, 'Jim', 'Doe', '25.50', '2021-06-15', '0703220499'), ('jimmy@gmail.com', 5, 'Jimmy', 'Doe', '25.50', '2021-06-15', '0703220499');
+
+SELECT * FROM employees;
