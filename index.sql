68c43550e96440658eaec0e1ee0c864d19299024
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-12 12:47:55 +0300

    Unique constraint 2

--- index.sql
+++ index.sql
@@ -1,7 +1,9 @@
-CREATE TABLE products(
+INSERT INTO products
 
-    product_id INT, 
-    product_name VARCHAR(25) UNIQUE,
-    product_price DECIMAL(4, 2),
-
-)
+VALUES
+(1, 'iPhone 12', 999.99),
+(2, 'Samsung Galaxy S21', 799.99),
+(3, 'Google Pixel 5', 699.99),
+(4, 'OnePlus 9', 729.99),
+(5, 'Sony Xperia 1 II', 949.99),
+(6, 'iPhone 12', 999.99);
