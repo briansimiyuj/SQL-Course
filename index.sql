e4688540adb1a82123b6c902146270849589b269
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-12 15:29:32 +0300

    Check constraint

--- index.sql
+++ index.sql
@@ -1,4 +1,8 @@
-INSERT INTO products
+CREATE TABLE productss(
 
-VALUES
-(7, 'Wireless Mouse') 
+    product_id INT,
+    product_name VARCHAR(25),
+    product_price DECIMAL(10, 2),
+    CONSTRAINT chk_product_price CHECK (product_price > 0)
+
+)
