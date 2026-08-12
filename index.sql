87fd7060e0834168d4851f9c2b3e003f8f3bbc83
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-12 13:02:00 +0300

    Not null constraint

--- index.sql
+++ index.sql
@@ -1,9 +1,3 @@
-INSERT INTO products
+ALTER TABLE products
 
-VALUES
-(1, 'iPhone 12', 999.99),
-(2, 'Samsung Galaxy S21', 799.99),
-(3, 'Google Pixel 5', 699.99),
-(4, 'OnePlus 9', 729.99),
-(5, 'Sony Xperia 1 II', 949.99),
-(6, 'iPhone 12', 999.99);
+MODIFY COLUMN product_price DECIMAL(10, 2) NOT NULL;
