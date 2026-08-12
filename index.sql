a01134734f672c3416cf963c543fed4283fd50fc
Brian Simiyu <briansimiyuj@gmail.com>
2026-08-12 15:37:46 +0300

    Check constraint

--- index.sql
+++ index.sql
@@ -1,8 +1,3 @@
-CREATE TABLE productss(
+INSERT INTO TABLE productss
 
-    product_id INT,
-    product_name VARCHAR(25),
-    product_price DECIMAL(10, 2),
-    CONSTRAINT chk_product_price CHECK (product_price > 0)
-
-)
+VALUES (1, 'Iphone 13')
